432f173c7c47104cb3c87129000333028aae7ce6
simon <simon.bure@mailo.com>
2026-02-22 12:47:17 +0100

    merger pending finishing

--- src/pdf/merger.rs
+++ src/pdf/merger.rs
@@ -3,10 +3,118 @@ use lopdf::content::{Content, Operation};
 use std::collections::BTreeMap;
 
 pub fn merge(doc1: &mut Document, doc2: &mut Document) -> Result<Document, Box<dyn std::error::Error>> {
+        // Stack of documents to merge
+    let documents = vec![doc1, doc2];
+
+    // Define a starting `max_id` (will be used as start index for object_ids).
+    let mut max_id = 1;
+    let mut pagenum = 1;
+
     // Collect all Documents Objects grouped by a map
     let mut documents_pages = BTreeMap::new();
     let mut documents_objects = BTreeMap::new();
     let mut document = Document::with_version("1.5");
 
+    for mut doc in documents {
+        let mut first = false;
+        doc.renumber_objects_with(max_id);
 
+        max_id = doc.max_id + 1;
+
+        documents_pages.extend(
+            doc
+            .get_pages()
+            .into_iter()
+            .map(|(_, object_id)| {
+                if !first {
+                    let bookmark = Bookmark::new(String::from(format!("Page_{}", pagenum)), [0.0, 0.0, 1.0], 0, object_id);
+                    document.add_bookmark(bookmark, None);
+                    pagenum += 1;
+                }
+                
+                (
+                    object_id,
+                    doc.get_object(object_id).unwrap().to_owned(),
+                )
+            })
+            .collect::<BTreeMap<ObjectId, Object>>(),
+        );
+        documents_objects.extend(doc.objcets);
+    }
+
+    // "Catalog" and "Pages" are mandatory.
+    let mut catalog_object: Option<(ObjectId, Object)> = None;
+    let mut pages_object: Option<(ObjectId, Object)> = None;
+
+    // Process all objects except "Page" type
+    for (object_id, object) in documents_objects.iter() {
+        // We have to ignore "Page" (as are processed later), "Outlines" and "Outline" objects.
+        // All other objects should be collected and inserted into the main Document.
+        match object.type_name().unwrap_or(b"") {
+            b"Catalog" => {
+                // Collect a first "Catalog" object and use it for the future "Pages".
+                catalog_object = Some((
+                    if let Some((id, _)) = catalog_object {
+                        id
+                    } else {
+                        *object_id
+                    },
+                    object.clone(),
+                ));
+            }
+            b"Pages" => {
+                // Collect and update a first "Pages" object and use it for the future "Catalog"
+                // We have also to merge all dictionaries of the old and the new "Pages" object
+                if let Ok(dictionary) = object.as_dict() {
+                    let mut dictionary = dictionary.clone();
+                    if let Some((_, ref object)) = pages_object {
+                        if let Ok(old_dictionary) = object.as_dict() {
+                            dictionary.extend(old_dictionary);
+                        }
+                    }
+
+                    pages_object = Some((
+                        if let Some((id, _)) = pages_object {
+                            id
+                        } else {
+                            *object_id
+                        },
+                        Object::Dictionary(dictionary),
+                    ));
+                }
+            }
+            b"Page" => {}     // Ignored, processed later and separately
+            b"Outlines" => {} // Ignored, not supported yet
+            b"Outline" => {}  // Ignored, not supported yet
+            _ => {
+                document.objects.insert(*object_id, object.clone());
+            }
+        }
+    }
+
+    // If no "Pages" object found, abort.
+    if pages_object.is_none() {
+        println!("Pages root not found.");
+
+        return Ok(());
+    }
+
+    // Iterate over all "Page" objects and collect into the parent "Pages" created before
+    for (object_id, object) in documents_pages.iter() {
+        if let Ok(dictionary) = object.as_dict() {
+            let mut dictionary = dictionary.clone();
+            dictionary.set("Parent", pages_object.as_ref().unwrap().0);
+
+            document
+                    .objects
+                    .insert(*object_id, Object::Dictionary(dictionary));
+        }
+    }
+
+    // If no "Catalog" found, abort.
+    if catalog_object.is_none() {
+        println!("Catalog root not found.");
+
+        return Ok(());
+    }
 }
